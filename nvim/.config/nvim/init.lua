c4479131fdb7a13de707d4dc40b788523b26efed
Rudiney Franceschi <rudi.atp@gmail.com>
2024-09-07 09:55:56 -0300

    replace tsserver by ts_ls from the deprecat msg

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -246,7 +246,7 @@ require("bufferline").setup({
 -- LSPs stuff
 require("mason").setup()
 require("mason-lspconfig").setup({
-  -- ensure_installed = { "lua_ls", "solargraph", "rubocop", "biome", "eslint", "tsserver" },
+  ensure_installed = { "lua_ls", "solargraph", "rubocop", "biome", "eslint", "ts_ls" },
 })
 
 local cmp = require("cmp")
