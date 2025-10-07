fdea7c932fcc1b0ba5a35ad293ad6b174f70dfed
Rudiney Franceschi <rudi.atp@gmail.com>
2025-10-07 10:02:08 -0300

    Replace Fzf back to Telescope

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -17,24 +17,33 @@ return {
     end,
   },
   {
-    'ibhagwan/fzf-lua',
-    cmd = 'FzfLua',
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'Telescope',
     keys = {
-      { '<Leader>y', function() require('fzf-lua').files() end, desc = 'Find files' },
-      { '<Leader>Y', function() require('fzf-lua').oldfiles() end, desc = 'Find recent files' },
-      { '<Leader>f', function() require('fzf-lua').live_grep() end, desc = 'Live grep' },
-      { '<Leader>b', function() require('fzf-lua').buffers() end, desc = 'Find buffers' },
-      { '<Leader>gs', function() require('fzf-lua').git_status() end, desc = 'Git status' },
-      { '"', function() require('fzf-lua').registers() end, desc = 'Open registers' },
-      { '<Leader>m', function() require('fzf-lua').marks() end, desc = 'Find marks' },
-      { '<Leader>d', function() require('fzf-lua').lsp_document_diagnostics() end, desc = 'Buffer diagnostics' },
-      { '<Leader>D', function() require('fzf-lua').lsp_workspace_diagnostics() end, desc = 'Workspace diagnostics' },
+      { '<Leader>y', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
+      { '<Leader>Y', function() require('telescope.builtin').oldfiles() end, desc = 'Find recent files' },
+      { '<Leader>f', function() require('telescope.builtin').live_grep() end, desc = 'Live grep' },
+      { '<Leader>b', function() require('telescope.builtin').buffers() end, desc = 'Find buffers' },
+      { '<Leader>gs', function() require('telescope.builtin').git_status() end, desc = 'Git status' },
+      { '"', function() require('telescope.builtin').registers() end, desc = 'Open registers' },
+      { '<Leader>m', function() require('telescope.builtin').marks() end, desc = 'Find marks' },
+      { '<Leader>d', function() require('telescope.builtin').diagnostics({ bufnr = 0 }) end, desc = 'Buffer diagnostics' },
+      { '<Leader>D', function() require('telescope.builtin').diagnostics() end, desc = 'Workspace diagnostics' },
     },
     config = function()
-      require('fzf-lua').setup({
-        "hide",
-        files = { rg_opts = "--sort=accessed --color=never --files --glob !*.rbi" },
-        live_grep = { rg_opts = "--glob '!*.rbi'" }
+      require('telescope').setup({
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = {
+            prompt_position = "top",
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--sortr", "accessed" },
+          },
+        },
       })
     end,
   },
